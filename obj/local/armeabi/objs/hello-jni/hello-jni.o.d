4554ff20d4e8fba796b0a35599ed9d54f09b8c99
lvcha <997027407@qq.com>
2020-03-29 18:21:33 +0800

    first

---
+++ obj/local/armeabi/objs/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs/hello-jni/hello-jni.o: jni/hello-jni.c \
+ /Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/string.h \
+ /Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ /Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h
+
+/Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/string.h:
+
+/Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+/Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/Users/lvcha/Documents/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h:
